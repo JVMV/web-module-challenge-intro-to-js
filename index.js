5e4c83de6d55f7b7e1976c98a95fa9229cdc6daf
Justin Abellera <justin.c.abellera@gmail.com>
2022-06-06 22:40:41 -0700

    fourth update

--- index.js
+++ index.js
@@ -254,9 +254,14 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
-}
+function annoyingSong(annNum){
+return annNum + " bottles of soda on the wall, " + annNum + " bottles of soda, take one down pass it around " + (annNum - 1) + " bottles of soda on the wall"
+};
+
+for(let i = 0; i > 1; i--) {
+  console.log(annoyingSong(i));
+  i--;
+};
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -274,9 +279,23 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+if(score < 60) {
+  return 'you got an F';
+} else if(score <= 69) {
+  return 'you got a D';
+} else if(score <= 79) {
+  return 'you got a C';
 }
+else if(score <= 89) {
+  return 'you got a B';
+}
+else if(score <= 100) {
+  return 'you got an A';
+} else {
+  return 'Enter a number between 1-100';
+}
+};
 
 
 
